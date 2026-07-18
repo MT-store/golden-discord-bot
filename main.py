621118c8658d65f171f6f291e19e907c0ad37ef1
MT-store <mt.blockspin@gmail.com>
2026-07-19 00:04:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="-", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"تم تشغيل البوت: {bot.user}")
+
+bot.run("TOKEN")
