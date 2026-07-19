6db4da56e6c706656dd869037e0cdbce5e2570e2
MT-store <mt.blockspin@gmail.com>
2026-07-19 10:01:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import os
 from flask import Flask
 from threading import Thread
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Bot is running"
+
 
 def run():
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 10000))
     )
+
 
 Thread(target=run).start()
 
@@ -24,7 +28,16 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(
+
+class MyBot(commands.Bot):
+
+    async def setup_hook(self):
+        await self.load_extension("points")
+        await self.load_extension("auto_reply")
+        await self.load_extension("roulette")
+
+
+bot = MyBot(
     command_prefix="-",
     intents=intents
 )
@@ -35,15 +48,7 @@ async def on_ready():
     print(f"تم تشغيل البوت: {bot.user}")
 
 
-async def setup_hook():
-    await bot.load_extension("points")
-    await bot.load_extension("auto_reply")
-    await bot.load_extension("roulette")
-
-
-bot.setup_hook = setup_hook
-
-
 print("جاري تشغيل البوت...")
+
 
 bot.run(os.environ["TOKEN"])
